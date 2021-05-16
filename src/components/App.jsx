aeb882accdbce661be6de4169f69e33b3562b26b
akash02ab <akash02.ab@gmail.com>
2021-05-16 22:09:15 +0530

    background image added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Transactions from './Transactions';
 import Form from './Form';
 import Invoice from '../contexts/Invoice';
 
+
 const API = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false";
 
 const App = () => {
